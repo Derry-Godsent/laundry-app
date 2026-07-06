32396935561977ec792615ffe3eee32805edefe1
Derry Kay <derrickbright164@gmail.com>
2026-07-06 02:57:13 +0000

    Remove legacy Chakra UI files causing build errors

--- src/components/charts/index.tsx
+++
@@ -1,6 +0,0 @@
-export { default as InventoryStatusChart } from "./InventoryStatusChart";
-export { default as RevenueChart } from "./RevenueChart";
-export { default as OrderStatusChart } from "./OrderStatusChart";
-export { default as StaffChart } from "./StaffChart";
-export { default as CustomerChart } from "./CustomerChart";
-export { default as InventoryMonthlyChart } from "./InventoryMonthlyChart";
